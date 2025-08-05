baf8e1a2d367589dc342c848c24edfcb43ab481a
Raghvendra <raghvendra3004008@gmail.com>
2025-08-05 15:40:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+nav.classList.remove('active');
+    })
+}
+
+var deadline = new Date("December 7, 2024 24:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(10000* 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("demo").innerHTML =  hours + ":" + minutes + ":" + seconds ;
+    
+}, 1000);
